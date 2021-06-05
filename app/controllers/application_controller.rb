600754875040e3d7de29eda19c8a80743772cf0c
Ignacio Moran <moran.ua@gmail.com>
2021-06-05 17:59:52 -0400

    before add ROLE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_pirmitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_pirmitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) #se agregan los parametros del schema (email viene por defecto)
+  end
 end
